b9261c96117db6105fbed4e8023366a33aa32990
yomzeew <yomzeew@gmail.com>
2025-01-18 00:40:02 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import authRoute from './routes/userRoutes';
 import carListingRoute from './routes/carListingRoutes'
 import uploadRoutes from './routes/uploadRoutes'
 import bodyParser from 'body-parser';
-import serverless from 'serverless-http';
+
 
 dotenv.config();
 const app = express();
@@ -53,4 +53,3 @@ app.listen(port, async() => {
   Log.success(`API is Alive and running 🚀 on port ${port}`);
   await sequelize.sync({ alter: false });
 });
-export const handler = serverless(app);
