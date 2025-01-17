0b0ea0ba88b6536c16e9f79a52d6ebc4644b73c7
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2025-01-17 23:03:00 +0100

    Deleted js files

--- src/enums/order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderStatus = void 0;
-var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["Pending"] = "Pending";
-    OrderStatus["Completed"] = "Compeleted";
-    OrderStatus["Confirmed"] = "Confirmed"; //To be updated by the user when item is received
-})(OrderStatus || (exports.OrderStatus = OrderStatus = {}));
