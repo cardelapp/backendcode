9f5bf9433c2e3d90cf9dcda0d619aeaa888e17f8
yomzeew <yomzeew@gmail.com>
2025-01-18 00:25:33 +0100

    my update

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import authRoute from './routes/userRoutes';
 import carListingRoute from './routes/carListingRoutes'
 import uploadRoutes from './routes/uploadRoutes'
 import bodyParser from 'body-parser';
+import ServerlessHttp from "serverless-http";
 
 dotenv.config();
 const app = express();
@@ -52,3 +53,4 @@ app.listen(port, async() => {
   Log.success(`API is Alive and running 🚀 on port ${port}`);
   await sequelize.sync({ alter: false });
 });
+export const handler = ServerlessHttp(app);
